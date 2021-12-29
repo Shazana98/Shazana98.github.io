7ae0ed5ab8fc18f5367a78d17274ba97c7f79da6
Shazana Affandi <37362149+Shazana98@users.noreply.github.com>
2021-12-29 12:18:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Asisstant Content Manager"],
+        strings: ["Website Manager"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
